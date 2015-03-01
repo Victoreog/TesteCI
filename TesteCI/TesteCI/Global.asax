4851daecc83936401e840266650f3812e392ad1e
victoreog - Victor Emiliano <victoreog2005@hotmail.com>
2015-03-01 10:54:40 -0300

    Criação do projeto

---
+++ TesteCI/TesteCI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TesteCI.MvcApplication" Language="C#" %>
---
+++ TesteCI/TesteCI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TesteCI.Startup))]
+namespace TesteCI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
